0c646e968562f21b1924b47837c32ce1adfc6338
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2021-04-25 13:51:31 +0200

    Improve performance of AppInitHook.dll

--- AppInitHook/AppInitHook.cpp
+++ AppInitHook/AppInitHook.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 
+wchar_t szDispatcherPath[MAX_PATH];
+
 BOOL WINAPI DllMain(
 	_In_ HINSTANCE hinstDLL,
 	_In_ DWORD     fdwReason,
@@ -8,12 +10,16 @@ BOOL WINAPI DllMain(
 {
 	if (fdwReason == DLL_PROCESS_ATTACH)
 	{
+		GetModuleFileNameW(hinstDLL, szDispatcherPath, _countof(szDispatcherPath));
+		for (int i = lstrlenW(szDispatcherPath) - 1; i > 0 && szDispatcherPath[i] != '\\'; i--)
+			szDispatcherPath[i] = L'\0';
 		OutputDebugStringA("[AppInitHook] The bomb has been planted!\n");
 #ifdef _WIN64
-		LoadLibraryW(L"AppInitDispatcher_x64.dll");
+		lstrcatW(szDispatcherPath, L"AppInitDispatcher_x64.dll");
 #else
-		LoadLibraryW(L"AppInitDispatcher_x86.dll");
+		lstrcatW(szDispatcherPath, L"AppInitDispatcher_x86.dll");
 #endif //_WIN64
+		LoadLibraryW(szDispatcherPath);
 	}
 	return FALSE;
 }
