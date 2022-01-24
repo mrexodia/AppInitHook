11ce65b058d8842acdbcd7e15165d7e7cf0ce06e
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2022-01-25 00:04:41 +0100

    Implement OutputDebugStringA without exceptions

--- Libraries/HookDll/HookDll.cpp
+++ Libraries/HookDll/HookDll.cpp
@@ -9,10 +9,11 @@ extern "C" __declspec(dllexport) void inject() { }
 
 void dprintf(const char* format, ...)
 {
-	static char dprintf_msg[66000];
+	static char dprintf_msg[1024];
 	va_list args;
 	va_start(args, format);
 	*dprintf_msg = 0;
+	//int len = 0;
 	auto len = vsnprintf_s(dprintf_msg, sizeof(dprintf_msg), format, args);
 	for (; len > 1; len--)
 	{
@@ -22,6 +23,7 @@ void dprintf(const char* format, ...)
 		else
 			break;
 	}
+	//DebugPrint
 	OutputDebugStringA(dprintf_msg);
 }
 
@@ -64,6 +66,227 @@ static BOOL WINAPI VirtualProtectSyscall(
 	return NT_SUCCESS(ntpvm_copy(GetCurrentProcess(), &lpAddress, &dwSize, flNewProtect, lpflOldProtect));
 }
 
+extern "C" NTSYSAPI PVOID RtlFreeSid(PSID Sid);
+
+extern "C" NTSYSAPI NTSTATUS RtlAllocateAndInitializeSid(
+	PSID_IDENTIFIER_AUTHORITY IdentifierAuthority,
+	UCHAR                     SubAuthorityCount,
+	ULONG                     SubAuthority0,
+	ULONG                     SubAuthority1,
+	ULONG                     SubAuthority2,
+	ULONG                     SubAuthority3,
+	ULONG                     SubAuthority4,
+	ULONG                     SubAuthority5,
+	ULONG                     SubAuthority6,
+	ULONG                     SubAuthority7,
+	PSID * Sid
+);
+
+extern "C"
+NTSYSAPI
+NTSTATUS
+RtlAddMandatoryAce(
+	_Inout_ PACL Acl,
+	_In_ ULONG AceRevision,
+	_In_ ULONG AceFlags,
+	_In_ PSID Sid,
+	_In_ UCHAR AceType,
+	_In_ ACCESS_MASK AccessMask
+);
+
+using _WORD = uint16_t;
+using _DWORD = uint32_t;
+
+static HANDLE CreateDBWinMutex()
+{
+	_ACL* v0; // rbx
+	_ACL* v1; // rdi
+	HANDLE hMutex; // r14
+	ULONG v3; // esi
+	ULONG v4; // esi
+	ULONG v5; // esi
+	_ACL* v6; // rax
+	ULONG v7; // esi
+	_ACL* v8; // rax
+	PSID Sid; // [rsp+68h] [rbp-39h] BYREF
+	PSID v11; // [rsp+70h] [rbp-31h] BYREF
+	PSID v12; // [rsp+78h] [rbp-29h] BYREF
+	PSID v13; // [rsp+80h] [rbp-21h] BYREF
+	SECURITY_ATTRIBUTES MutexAttributes; // [rsp+88h] [rbp-19h] BYREF
+	SECURITY_DESCRIPTOR SecurityDescriptor; // [rsp+A0h] [rbp-1h] BYREF
+	SID_IDENTIFIER_AUTHORITY ntAuthoritySid; // [rsp+C8h] [rbp+27h] BYREF
+	SID_IDENTIFIER_AUTHORITY worldAuthoritySid; // [rsp+D0h] [rbp+2Fh] BYREF
+	SID_IDENTIFIER_AUTHORITY v18; // [rsp+D8h] [rbp+37h] BYREF
+
+	*(_WORD*)&ntAuthoritySid.Value[4] = 0x500;
+	*(_DWORD*)ntAuthoritySid.Value = 0;
+	v0 = 0i64;
+	v1 = 0i64;
+	hMutex = 0i64;
+	*(_DWORD*)worldAuthoritySid.Value = 0;
+	*(_WORD*)&worldAuthoritySid.Value[4] = 0x100;
+	*(_DWORD*)v18.Value = 0;
+	*(_WORD*)&v18.Value[4] = 0x1000;
+	Sid = 0i64;
+	v11 = 0i64;
+	v12 = 0i64;
+	v13 = 0i64;
+	if (RtlAllocateAndInitializeSid(&ntAuthoritySid, 1u, 0x12u, 0, 0, 0, 0, 0, 0, 0, &Sid) >= 0
+		&& RtlAllocateAndInitializeSid(&ntAuthoritySid, 2u, 0x20u, 0x220u, 0, 0, 0, 0, 0, 0, &v11) >= 0
+		&& RtlAllocateAndInitializeSid(&worldAuthoritySid, 1u, 0, 0, 0, 0, 0, 0, 0, 0, &v12) >= 0
+		&& RtlAllocateAndInitializeSid(&v18, 1u, 0x1000u, 0, 0, 0, 0, 0, 0, 0, &v13) >= 0
+		&& RtlCreateSecurityDescriptor(&SecurityDescriptor, 1u) >= 0)
+	{
+		v3 = RtlLengthSid(v12);
+		v4 = RtlLengthSid(v11) + v3;
+		v5 = RtlLengthSid(Sid) + 32 + v4;
+		v6 = (_ACL*)GlobalAlloc(0, v5);
+		v0 = v6;
+		if (v6)
+		{
+			if (RtlCreateAcl(v6, v5, ACL_REVISION) >= 0
+				&& RtlAddAccessAllowedAce(v0, ACL_REVISION, 0x120001u, v12) >= 0
+				&& RtlAddAccessAllowedAce(v0, ACL_REVISION, 0x1F0001u, Sid) >= 0
+				&& RtlAddAccessAllowedAce(v0, ACL_REVISION, 0x1F0001u, v11) >= 0
+				&& RtlSetDaclSecurityDescriptor(&SecurityDescriptor, 1u, v0, 0) >= 0)
+			{
+				v7 = RtlLengthSid(v13) + 16;
+				v8 = (_ACL*)GlobalAlloc(0, v7);
+				v1 = v8;
+				if (v8)
+				{
+					if (RtlCreateAcl(v8, v7, ACL_REVISION) >= 0
+						&& RtlAddMandatoryAce(v1, ACL_REVISION, 0, v13, 0x11u, 1u) >= 0
+						&& RtlSetSaclSecurityDescriptor(&SecurityDescriptor, 1u, v1, 0) >= 0)
+					{
+						MutexAttributes.nLength = 24;
+						MutexAttributes.lpSecurityDescriptor = &SecurityDescriptor;
+						MutexAttributes.bInheritHandle = 0;
+						hMutex = CreateMutexExW(&MutexAttributes, L"DBWinMutex", 0, 0x2120001u);
+					}
+				}
+			}
+		}
+	}
+	if (Sid)
+		RtlFreeSid(Sid);
+	if (v11)
+		RtlFreeSid(v11);
+	if (v12)
+		RtlFreeSid(v12);
+	if (v13)
+		RtlFreeSid(v13);
+	if (v0)
+		GlobalFree(v0);
+	if (v1)
+		GlobalFree(v1);
+	return hMutex;
+}
+
+static void OutputDebugStringMeme(LPCSTR lpOutputString)
+{
+	LPCSTR v1; // rdx
+	__int64 stringToPrintLength; // rax
+	char* pDBWinBuffer; // r15
+	HANDLE hDBWinBufferReadyEvent; // r14
+	HANDLE hDBWinDataReadyEvent; // rdi
+	HANDLE DBWinMutex; // rax
+	HANDLE hDBWinBufferMapping; // rax MAPDST
+	unsigned __int64 len; // rbx MAPDST
+	int tempLen; // eax
+	__int64 tempLen1; // r13
+	int bufferLen; // eax
+	DWORD LastError; // [rsp+40h] [rbp-298h]
+	LPCSTR stringToPrint; // [rsp+80h] [rbp-258h] MAPDST
+	ULONG_PTR Arguments[2]; // [rsp+88h] [rbp-250h] BYREF
+	char temp[512]; // [rsp+A0h] [rbp-238h] BYREF
+
+	static HANDLE hDBWinMutex;
+	static BOOL hasDBWinMutex;
+
+	v1 = "";
+	if (lpOutputString)
+		v1 = lpOutputString;
+	stringToPrint = v1;
+
+	hDBWinBufferMapping = 0i64;                   // __except
+	pDBWinBuffer = 0i64;
+	hDBWinBufferReadyEvent = 0i64;
+	hDBWinDataReadyEvent = 0i64;
+	LastError = GetLastError();
+	if (!hDBWinMutex && !hasDBWinMutex)
+	{
+		DBWinMutex = CreateDBWinMutex();
+		if (DBWinMutex)
+		{
+			if (_InterlockedCompareExchange64((volatile signed __int64*)&hDBWinMutex, (signed __int64)DBWinMutex, 0i64))
+				CloseHandle(DBWinMutex);
+		}
+		else
+		{
+			hasDBWinMutex = 1;
+		}
+	}
+	if (hDBWinMutex && (WaitForSingleObjectEx(hDBWinMutex, 10000u, 0) & 0xFFFFFF7F) == 0)
+	{
+		hDBWinBufferMapping = OpenFileMappingW(2u, 0, L"DBWIN_BUFFER");
+		if (hDBWinBufferMapping)
+		{
+			pDBWinBuffer = (char*)MapViewOfFile(hDBWinBufferMapping, 6u, 0, 0, 0);
+			//pDBWinBuffer = (char*)MapViewOfFileExNuma(hDBWinBufferMapping, 6u, 0, 0, 0i64, 0i64, 0xFFFFFFFF);
+			if (pDBWinBuffer)
+			{
+				hDBWinBufferReadyEvent = OpenEventA(0x100000u, 0, "DBWIN_BUFFER_READY");
+				if (hDBWinBufferReadyEvent)
+					hDBWinDataReadyEvent = OpenEventA(2u, 0, "DBWIN_DATA_READY");
+			}
+		}
+		if (!hDBWinDataReadyEvent)
+			ReleaseMutex(hDBWinMutex);
+	}
+	len = -1i64;
+	do
+		++len;
+	while (stringToPrint[len]);
+	while (len)
+	{
+		if (!hDBWinDataReadyEvent || WaitForSingleObjectEx(hDBWinBufferReadyEvent, 10000u, 0))
+		{
+			tempLen = 511;
+			if (len < 511)
+				tempLen = len;
+			tempLen1 = tempLen;
+			memcpy(temp, stringToPrint, tempLen);
+			temp[tempLen1] = 0;
+			DbgPrint("%s", temp);
+		}
+		else
+		{
+			*(_DWORD*)pDBWinBuffer = GetCurrentProcessId();
+			bufferLen = 4091;
+			if (len < 4091)
+				bufferLen = len;
+			tempLen1 = bufferLen;
+			memcpy(pDBWinBuffer + 4, stringToPrint, bufferLen);
+			pDBWinBuffer[tempLen1 + 4] = 0;
+			SetEvent(hDBWinDataReadyEvent);
+		}
+		stringToPrint += tempLen1;
+		len -= tempLen1;
+	}
+	if (pDBWinBuffer)
+		UnmapViewOfFile(pDBWinBuffer);
+	if (hDBWinDataReadyEvent)
+	{
+		ReleaseMutex(hDBWinMutex);
+		CloseHandle(hDBWinDataReadyEvent);
+	}
+	if (hDBWinBufferReadyEvent)
+		CloseHandle(hDBWinBufferReadyEvent);
+	if (hDBWinBufferMapping)
+		CloseHandle(hDBWinBufferMapping);
+}
+
 // Call this from your DllMain to use the HOOK macros
 BOOL WINAPI HookDllMain(
 	_In_ HINSTANCE hinstDLL,
@@ -73,12 +296,29 @@ BOOL WINAPI HookDllMain(
 {
 	if (fdwReason == DLL_PROCESS_ATTACH)
 	{
+		// There is a weird behavior when calling OutputDebugString from certain functions
+		// While unwinding the SRW lock on the RtlpInvertedFunctionTable is already held
+		// and it deadlocks.
+		// As a workaround: reimplement the __except handler from OutputDebugStringA
+		AddVectoredExceptionHandler(1, [](struct _EXCEPTION_POINTERS* ExceptionInfo) -> LONG
+		{
+			auto er = ExceptionInfo->ExceptionRecord;
+			if (er->ExceptionCode == DBG_PRINTEXCEPTION_C && er->NumberParameters == 2)
+			{
+				auto length = er->ExceptionInformation[0]; // seems to be ignored?
+				auto str = (const char*)er->ExceptionInformation[1];
+				OutputDebugStringMeme(str);
+				return EXCEPTION_CONTINUE_EXECUTION;
+			}
+			return EXCEPTION_CONTINUE_SEARCH;
+		});
+
 		// Super awful hack
 		ntpvm_copy = (decltype(NtProtectVirtualMemory)*)VirtualAlloc(0, 0x1000, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 		memcpy(ntpvm_copy, GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtProtectVirtualMemory"), 32);
 		DWORD oldProtect;
 		VirtualProtect(ntpvm_copy, 0x1000, PAGE_EXECUTE, &oldProtect);
-		MH_VirtualProtect = VirtualProtectSyscall;
+		//MH_VirtualProtect = VirtualProtectSyscall;
 
 #ifdef _WIN64
 		auto tlsIndex = TlsAlloc();
